92fbf8fa2cce04b7c3cb2130586ed760fceb8715
suassuna <suassuna.paulo@gmail.com>
2017-11-18 17:21:42 -0300

    Added transactions csv file reading and parsing.

--- lib/budget.ex
+++ lib/budget.ex
@@ -1,18 +1,52 @@
 defmodule Budget do
-  @moduledoc """
-  Documentation for Budget.
-  """
+  alias NimbleCSV.RFC4180, as: CSV
+  def list_transactions do
+    File.read!("lib/transactions.csv")
+    |> parse
+    |> filter
+    |> normalize
+    |> sort
+    |> print
+  end
 
-  @doc """
-  Hello world.
+  defp parse(string) do
+    string
+    |> String.replace("\r", "")
+    |> CSV.parse_string
+  end
 
-  ## Examples
+  defp filter(rows) do
+    Enum.map(rows, &Enum.drop(&1, 1))
+  end
 
-      iex> Budget.hello
-      :world
+  defp normalize(rows) do
+    Enum.map(rows, &parse_amount(&1))
+  end
 
-  """
-  def hello do
-    :world
+  defp parse_amount([date, description, amount]) do
+    [date, description, parse_to_float(amount)]
+  end
+
+  defp parse_to_float(string) do
+    string
+    |> String.to_float
+    |> abs
+  end
+
+  defp sort(rows) do
+    Enum.sort(rows, &sort_asc_by_amount(&1, &2))
+  end
+
+  defp sort_asc_by_amount([_, _, prev], [_, _, next]) do
+    prev < next
+  end
+
+  defp print(rows) do
+    IO.puts "\nTransactions:"
+    Enum.each(rows, &print_to_console(&1))
+  end
+
+  defp print_to_console([date, description, amount]) do
+    IO.puts "#{date} #{description} \t$#{:erlang.float_to_binary(amount, decimals: 2)}"
   end
 end
--- mix.exs
+++ mix.exs
@@ -7,8 +7,7 @@ defmodule Budget.Mixfile do
       version: "0.1.0",
       elixir: "~> 1.5",
       start_permanent: Mix.env == :prod,
-      deps: deps()
-    ]
+      deps: deps(), default_task: "list_transactions"]
   end
 
   # Run "mix help compile.app" to learn about applications.
@@ -20,9 +19,6 @@ defmodule Budget.Mixfile do
 
   # Run "mix help deps" to learn about dependencies.
   defp deps do
-    [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
-    ]
+    [{:nimble_csv, "~> 0.1.0"}]
   end
 end
